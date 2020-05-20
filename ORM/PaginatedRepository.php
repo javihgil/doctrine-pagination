28ed70f3d7a23340eba8f6f6cb8bdd28e17e5416
Javi H. Gil <javihgil@gmail.com>
2020-05-20 11:56:18 +0200

    Paginated repository findBy trait

--- ORM/PaginatedRepository.php
+++ ORM/PaginatedRepository.php
@@ -10,21 +10,6 @@ use Doctrine\ORM\EntityRepository;
 class PaginatedRepository extends EntityRepository implements PaginatedRepositoryInterface
 {
     use PaginatedRepositoryTrait;
+    use PaginatedRepositoryFindByTrait;
     use FilterRepositoryTrait;
-
-    /**
-     * {@inheritdoc}
-     *
-     * @deprecated this method will be removed in this repository
-     */
-    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
-    {
-        if ($offset !== null && $limit !== null && $limit > 0) {
-            $page = ceil($offset/$limit) + 1;
-        } else {
-            $page = 1;
-        }
-
-        return $this->findPageBy($page, $limit !== null && $limit > 0 ? $limit : -1, $criteria, $orderBy);
-    }
 }
---
+++ ORM/PaginatedRepositoryFindByTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Jhg\DoctrinePagination\ORM;
+
+trait PaginatedRepositoryFindByTrait
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated this method will be removed in this repository
+     */
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        if ($offset !== null && $limit !== null && $limit > 0) {
+            $page = ceil($offset/$limit) + 1;
+        } else {
+            $page = 1;
+        }
+
+        return $this->findPageBy($page, $limit !== null && $limit > 0 ? $limit : -1, $criteria, $orderBy);
+    }
+}
